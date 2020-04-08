c8611e024091a7b92306eb1b710f02bed880faae
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-04-08 02:18:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const { Client, Collection } = require("discord.js");
 const TOKEN = require("./config.json").token;
 const mongoose = require("mongoose");
-
-const client = new Client({
-  disableEveryone: true
-});
+const client = new Client({ disableEveryone: true});
 client.mongoose = require("./utils/mongoose.js");
 client.afk = new Map();
-
 ["commands", "aliases"].forEach(x => (client[x] = new Collection()));
-["command", "event"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
-
+["command", "event"].forEach(handler => {require(`./handlers/${handler}`)(client);});
 client.mongoose.init();
 client.login(TOKEN);
