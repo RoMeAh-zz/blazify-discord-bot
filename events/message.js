0da96ee2aafc6774032baf61369fed2dd5bebfed
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-04-12 09:19:04 -0700

    Unecessary captcha files

--- events/message.js
+++ events/message.js
@@ -60,44 +60,7 @@ module.exports = async (client, message, member) => {
       }
     })
   }
-  if (enableCaptchaS === true) {
-    const captcha = await createCaptcha();
-    try {
-        const msg = await member.send('You have 60 seconds to solve the captcha', {
-            files: [{
-                attachment: `${__dirname}/captchas/${captcha}.png`,
-                name: `${captcha}.png`
-            }]
-        });
-        try {
-            const filter = m => {
-                if(m.author.bot) return;
-                if(m.author.id === member.id && m.content === captcha) return true;
-                else {
-                    m.channel.send('You entered the captcha incorrectly.');
-                    return false;
-                }
-            };
-            const response = await msg.channel.awaitMessages(filter, { max: 1, time: 3600000, errors: ['time']});
-            if(response) {
-                await msg.channel.send('You have verified yourself!');
-                await member.addRole("690664505037946951");
-                await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
-                    .catch(err => console.log(err));
-            }
-        }
-        catch(err) {
-            console.log(err);
-            await msg.channel.send('You did not solve the captcha correctly on time and you were kicked out of the server');
-            await member.kick();
-            await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
-                    .catch(err => console.log(err));
-        }
-    }
-    catch(err) {
-        console.log(err);
-    }
-}
+  
   if (enableXPS === true) {
     let addXP = Math.floor(Math.random() * 10 + 1);
 
@@ -154,7 +117,44 @@ module.exports = async (client, message, member) => {
     );
   }
   
-  
+  if (enableCaptchaS === true) {
+    const captcha = await createCaptcha();
+    try {
+        const msg = await member.send('You have 60 seconds to solve the captcha', {
+            files: [{
+                attachment: `${__dirname}/captchas/${captcha}.png`,
+                name: `${captcha}.png`
+            }]
+        });
+        try {
+            const filter = m => {
+                if(m.author.bot) return;
+                if(m.author.id === member.id && m.content === captcha) return true;
+                else {
+                    m.channel.send('You entered the captcha incorrectly.');
+                    return false;
+                }
+            };
+            const response = await msg.channel.awaitMessages(filter, { max: 1, time: 3600000, errors: ['time']});
+            if(response) {
+                await msg.channel.send('You have verified yourself!');
+                await member.addRole("690664505037946951");
+                await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
+                    .catch(err => console.log(err));
+            }
+        }
+        catch(err) {
+            console.log(err);
+            await msg.channel.send('You did not solve the captcha correctly on time and you were kicked out of the server');
+            await member.kick();
+            await fs.unlink(`${__dirname}/captchas/${captcha}.png`)
+                    .catch(err => console.log(err));
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
   if (message.channel.type === "dm")
     return message.channel.send("You are not supposed to DM Bots");
   
