b2d0de7668743f7eeafa99ed348057f192f4e047
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-05-19 00:30:42 +0530

    Tried to do some Class Based Javascript

---
+++ src/base/Command.js
@@ -0,0 +1,44 @@
+class Command {
+    constructor(client, options) {
+        this.help = {
+
+            name: options.name || null,
+
+            description: options.description || "No information specified.",
+
+            usage: options.usage || "",
+
+            category: options.category || "Information"
+        };
+        this.conf = {
+
+            permLevel: options.permLevel || 0,
+
+            permission: options.permission || "SEND_MESSAGES",
+
+            cooldown: options.cooldown || 1000,
+
+            aliases: options.aliases || [],
+
+            allowDMs: options.allowDMs || false
+
+        };
+        this.cooldown = new Set();
+
+    }
+
+    startCooldown(user) {
+        this.cooldown.add(user);
+        setTimeout(() => {
+            this.cooldown.delete(user);
+        }, this.conf.cooldown);
+    }
+    setMessage(message) {
+        this.message = message;
+    }
+    respond(message) {
+        this.message.channel.send(message);
+    }
+}
+
+module.exports = Command;
--- src/handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client) => {
-  let prompt = process.openStdin();
-  prompt.addListener("data", (res) => {
-    let x = res.toString().trim().split(/ +/g);
-    client.channels.get("696786895933538435").send(x.join(" "));
-  });
-};
