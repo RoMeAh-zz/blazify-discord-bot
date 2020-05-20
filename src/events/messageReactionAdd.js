f8e13dc996f726bea58f89eaf597632f4d65988c
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-05-20 11:37:31 +0530

    King Jesus

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -6,36 +6,37 @@ class messageReactionAdd {
       this.client = client;
     }
     
-   async run(client, reaction, message, user) {
-  let addMemberRole = (emojiRoleMappings) => {
-        if(emojiRoleMappings.hasOwnProperty(reaction.emoji.id)) {
-            let roleId = emojiRoleMappings[reaction.emoji.id];
-            let role = reaction.message.guild.roles.cache.get(roleId);
-            let member = reaction.message.guild.members.cache.get(user.id);
-            if(role && member) {
-                member.roles.add(role);
-            }
-        }
-    }
-    if(reaction.message.partial) {
-        await reaction.message.fetch();
-        let { id } = reaction.message;
-        try {
-            let msgDocument = await MessageModel.findOne({ messageId: id });
-            if(msgDocument) {
-                client.cachedMessageReactions.set(id, msgDocument.emojiRoleMappings);
-                let { emojiRoleMappings } = msgDocument;
-                addMemberRole(emojiRoleMappings);
-            }
-        }
-        catch(err) {
-            console.log(err);
-        }
-    }
-    else {
-        let emojiRoleMappings = client.cachedMessageReactions.get(reaction.message.id);
-        addMemberRole(emojiRoleMappings);
-    }
+   async run(client, messageReaction, user) {
+       console.log(messageReaction);
+//   let addMemberRole = (emojiRoleMappings) => {
+//         if(emojiRoleMappings.hasOwnProperty(reaction.emoji.id)) {
+//             let roleId = emojiRoleMappings[reaction.emoji.id];
+//             let role = reaction.message.guild.roles.cache.get(roleId);
+//             let member = reaction.message.guild.members.cache.get(user.id);
+//             if(role && member) {
+//                 member.roles.add(role);
+//             }
+//         }
+//     }
+//     if(reaction.message.partial) {
+//         await reaction.message.fetch();
+//         let { id } = reaction.message;
+//         try {
+//             let msgDocument = await MessageModel.findOne({ messageId: id });
+//             if(msgDocument) {
+//                 client.cachedMessageReactions.set(id, msgDocument.emojiRoleMappings);
+//                 let { emojiRoleMappings } = msgDocument;
+//                 addMemberRole(emojiRoleMappings);
+//             }
+//         }
+//         catch(err) {
+//             console.log(err);
+//         }
+//     }
+//     else {
+//         let emojiRoleMappings = client.cachedMessageReactions.get(reaction.message.id);
+//         addMemberRole(emojiRoleMappings);
+//     }
 }
 }
 module.exports = messageReactionAdd;
