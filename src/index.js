9f19b2d0f7c69baec376f3f042ce137c254223ae
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-05-28 01:23:18 +0530

    Delete index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const Client = require ( "./Lib/Base/BlazifyClient.js" );
-const client = new Client ({config : "../config" });
-client.login (client.config.token);
-client.loadCommands();
-client.loadEvents(client.config.paths.events);
