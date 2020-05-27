00772ae9b9b245ff6efbe2c6fb32be5bee1cbcf9
RoMeAh <62307220+RoMeAh@users.noreply.github.com>
2020-05-28 01:23:57 +0530

    Create index.js

---
+++ Src/index.js
@@ -0,0 +1,5 @@
+const Client = require ( "./Lib/Base/BlazifyClient.js" );
+const client = new Client ({config : "../config" });
+client.login (client.config.token);
+client.loadCommands();
+client.loadEvents(client.config.paths.events);
