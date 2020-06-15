0eecbb60587352f8b0d026845472e53c83561cf8
romeah <runa.ronit3129@gmail.com>
2020-06-15 05:08:19 -0400

    Fixed Music....

--- Src/Bot/Commands/Music/Play.ts
+++ Src/Bot/Commands/Music/Play.ts
@@ -1,8 +1,8 @@
 import { Command } from "discord-akairo";
 import { MessageEmbed, Message } from "discord.js";
-import { LavaClient } from "@anonymousg/lavajs";
-import {Queue} from "@anonymousg/lavajs/dist/managers/Queue";
-import { Utils } from "erela.js"
+import { Queue } from "@anonymousg/lavajs";
+import { Player } from "@anonymousg/lavajs";
+import {LavaClient} from "@anonymousg/lavajs/dist/managers/LavaClient";
 
 export default class Play extends Command {
     public constructor() {
@@ -30,44 +30,39 @@ export default class Play extends Command {
     }
     public async exec(message : Message, { song }: { song: string}) : Promise<any> {
         if(!message.member?.voice.channel) return message.util?.send(`${message.author} you are not present in any voice channel.`)
-        let Player = await this.client.lava?.spawnPlayer(LavaClient, {
-            guild: message.guild?.id ,
-            voiceChannel: message.member.voice.channel ,
-            textChannel: message.channel ,
-            deafen: true ,
-            trackRepeat: false ,
-            queueRepeat: false ,
-            skipOnError: true
-        });
-        message.util?.send(new MessageEmbed()
-            .setAuthor("*Joined Voice Channel and I am ready..*")
-            .setColor("GREEN")
-            .setDescription(`\n
+         let player =  await this.client.lava?.spawnPlayer ( this.client.lava , {
+        guild: message.guild ,
+        voiceChannel: message.member.voice.channel ,
+        textChannel: message.channel ,
+        deafen: true ,
+        trackRepeat: false ,
+        queueRepeat: false ,
+        skipOnError: true
+    } );
+    message.util?.send(new MessageEmbed()
+        .setAuthor("*Joined Voice Channel and I am ready..*")
+        .setColor("GREEN")
+        .setDescription(`\n
             \`Text Channel\`: ${message.channel}\n
             \`Voice Channel\`: ${message.member?.voice.channel}\n
             \`Guild Name\`: ${message.guild?.name}`)
-        );
-        let queue = new Queue(LavaClient)
-        let Song = await Player.lavaSearch(song, message.member, true)
-        if(!Song) return message.util?.send("No Song Found. Please Try Again")
-
+    );
+        let queue = new Queue(player)
+            let Song: any = await player.lavaSearch(song, message.member, true).catch((err: any) => {
+                message.util?.send ( err )
+            });
         await queue.add(Song)
-        await Player.play(Song)
-        if(Player.playState) {
-            message.util?.send(new MessageEmbed()
+            await player.play ();
+            await message.util?.send(new MessageEmbed()
                 .setAuthor(Song.author)
-                .setDescription(Song.title + `[${Song.title}](${Song.uri})`)
+                .setDescription(`[${Song.title}](${Song.uri}). Requested by ${Song.user.user.username}`)
                 .setImage(Song.thumbnail.max)
-                .setThumbnail(`message.guild?.iconURL ( {dynamic: true} )`)
+                .setThumbnail(message.author?.displayAvatarURL( {dynamic: true} ))
             )
-                }
         console.log ( Song )
-        if (!Player.playState) {
-            console.log("flop")
             setTimeout ( async () => {
-                await Player.destroy (message.guild?.id)
-                await message.util?.send ( "I was too long in the Voice Channel and no Song was Played" )
-            }, 10000 )
-        }
+                await player.destroy ( message.guild?.id )
+                await message.util?.send ( "Song Complete :)" )
+            } , Song.length )
     }
 }
