6db28767c79270e7041284147f7a1e1563577d5e
God <62307220+IamGoDsoIamBest@users.noreply.github.com>
2020-06-21 14:18:10 +0530

    Made ready for deployment

--- Src/Config.ts
+++ Src/Config.ts
@@ -1,5 +1,5 @@
-export let token: string = "Njk2NzU2MzIyODI1NDA0NDE2.XuXy4A.Obdm-wB_rnqrqjtrDfMzg13QHL8";
+export let token: string = "Njk2NzU2MzIyODI1NDA0NDE2.Xu8eXg.QiJdBHOU1q1QgAErdDSn6Fh9Xks";
 export let prefix: string = "<<";
 export let ownerID: Array<string> = [ '712607705184862278', '312149319769784321', '413620315508178955' ];
-export let secret: string = "8x8DCRNFAeoDcsr58JmBLUvgPqGlJ9lJ";
+export let secret: string = "aP-wHiT-5P0JMFjWyUNndgZ0MbP8xFwA";
 export let baseurl: string = "http://localhost:8080"
