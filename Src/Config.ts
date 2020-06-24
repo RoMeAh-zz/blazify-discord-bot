11b3153e9ac144fd18f51f77eb9f7fb45e650776
RoMeAh <runa.ronit3129@gmail.com>
2020-06-24 12:00:02 +0530

    Made Ready for Public Release

--- Src/Config.ts
+++
@@ -1,6 +0,0 @@
-export let token: string = "Njk2NzU2MzIyODI1NDA0NDE2.Xu8eXg.QiJdBHOU1q1QgAErdDSn6Fh9Xks";
-export let prefix: string = "<<";
-export let ownerID: Array<string> = [ '712607705184862278', '312149319769784321', '413620315508178955' ];
-export let secret: string = "aP-wHiT-5P0JMFjWyUNndgZ0MbP8xFwA";
-export let baseurl: string = "http://localhost:8080"
-export let mongodburl: string = "mongodb+srv://blazify:blazifyisbest@cluster0-pjlza.mongodb.net/blazify";
